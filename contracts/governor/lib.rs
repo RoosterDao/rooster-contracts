121a795c61caa735bbf072dbaa2edb5c909488cb
Wouter Godefroy <wgo@belitas.be>
2022-07-08 13:14:02 +0200

    partial chain_extension integration

--- contracts/governor/lib.rs
+++ contracts/governor/lib.rs
@@ -1,12 +1,82 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![feature(min_specialization)]
 
+use ink_env::{Environment, AccountId};
+use ink_lang as ink;
+
+#[derive(scale::Encode, scale::Decode, Debug)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum RCErrorCode {
+    Failed,
+}
+
+#[derive(scale::Encode, scale::Decode, Debug)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum RCError {
+    ErrorCode(RCErrorCode),
+}
+
+type CollectionId = u32;
+type NftId = u32;
+#[ink::chain_extension]
+pub trait RmrkExt {
+    type ErrorCode = RCErrorCode;
+
+    #[ink(extension = 1, returns_result = false)]
+    fn read_nft(caller_id: AccountId, collection_id: CollectionId, nft_id: NftId) -> bool;
+
+    #[ink(extension = 2, returns_result = false)]
+    fn mint_nft(contract_address: AccountId, owner: AccountId, collection_id: CollectionId, metadata: Vec<u8>) -> NftId;
+
+    #[ink(extension = 3, returns_result = false)]
+    fn create_collection(contract_address: AccountId, metadata: Vec<u8>, symbol: Vec<u8>) -> NftId;
+}
+
+impl From<RCErrorCode> for RCError {
+    fn from(error_code: RCErrorCode) -> Self {
+        Self::ErrorCode(error_code)
+    }
+}
+
+impl From<scale::Error> for RCError {
+    fn from(_: scale::Error) -> Self {
+        panic!("encountered unexpected invalid SCALE encoding")
+    }
+}
+
+impl ink_env::chain_extension::FromStatusCode for RCErrorCode {
+    fn from_status_code(status_code: u32) -> Result<(), Self> {
+        match status_code {
+            0 => Ok(()),
+            1 => Err(Self::Failed),
+            _ => panic!("encountered unknown status code"),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum CustomEnvironment {}
+
+impl Environment for CustomEnvironment {
+    const MAX_EVENT_TOPICS: usize = <ink_env::DefaultEnvironment as Environment>::MAX_EVENT_TOPICS;
+
+    type AccountId = <ink_env::DefaultEnvironment as Environment>::AccountId;
+    type Balance = <ink_env::DefaultEnvironment as Environment>::Balance;
+    type Hash = <ink_env::DefaultEnvironment as Environment>::Hash;
+    type BlockNumber = <ink_env::DefaultEnvironment as Environment>::BlockNumber;
+    type Timestamp = <ink_env::DefaultEnvironment as Environment>::Timestamp;
+
+    type ChainExtension = RmrkExt;
+}
 
 #[openbrush::contract]
 pub mod governor {
     use ink_storage::traits::SpreadAllocate;
     use ink_prelude::vec;
     use ink_prelude::vec::Vec;
+
+    use super::{RCError};
     
     use ink_prelude::string::{
         String,
@@ -748,7 +818,6 @@ pub mod governor {
         #[ink::test]
         fn delegate_works() {
             let accounts = accounts();
-            
 
             let mut governor = Governor::new(Some(String::from("Governor")),86400,604800,86400);
 
